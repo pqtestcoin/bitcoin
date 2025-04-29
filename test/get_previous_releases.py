3876c13b49b21a4b8d53eb94c8b340e7cb949c53
mraksoll <32709596+mraksoll4@users.noreply.github.com>
2025-04-29 22:25:33 +0300

    Update get_previous_releases.py

--- test/get_previous_releases.py
+++ test/get_previous_releases.py
@@ -200,7 +200,7 @@ def download_binary(tag, args) -> int:
 
 
 def build_release(tag, args) -> int:
-    githubUrl = "https://github.com/bitcoin/bitcoin"
+    githubUrl = "https://github.com/pqtestcoin/bitcoin"
     if args.remove_dir:
         if Path(tag).is_dir():
             shutil.rmtree(tag)
